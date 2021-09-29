c1fe90b6d0e40c8ec1e177800a4c772ac23f9689
rlad78 <goobysc@gmail.com>
2021-09-29 11:36:54 -0400

    plans for storing credentials

---
+++ cucmtoolkit/ciscoaxl/configs.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+ROOT_DIR: Path = Path(__file__).parent
+USERNAME_MAGIC_KEY: str = "73q0eWFaIE2JJw8FMNeX"
+URL_MAGIC_KEY: str = "8Cu16DGzNvunSsDNOTrO"
+DUMMY_KEY: str = "xlGoVnofkKjNSgnwA9Z7"
---
+++ cucmtoolkit/ciscoaxl/credentials.py
@@ -0,0 +1,48 @@
+from typing import Tuple
+import keyring
+import keyring.errors
+from cucmtoolkit.ciscoaxl.configs import *
+from stdiomask import getpass
+
+
+def get_credentials(enable_manual_entry=True, quiet=True) -> Tuple[str, str]:
+    if (username := keyring.get_password("cucmtoolkit", USERNAME_MAGIC_KEY)) is None:
+        if enable_manual_entry:
+            return credentials_from_input(quiet)
+        else:
+            return "", ""
+    elif (password := keyring.get_password("cucmtoolkit", username)) is None:
+        if enable_manual_entry:
+            return credentials_from_input(quiet)
+        else:
+            return username, ""
+    else:
+        return username, password
+
+
+def credentials_from_input(quiet=True) -> Tuple[str, str]:
+    username = input("CUCM username: ")
+    password = getpass(prompt="CUCM password: ")
+    write_credentials(username, password)
+    if not quiet:
+        print("Writing ENCRYPTED passwords to system keyring")
+    return username, password
+
+
+def write_credentials(username: str, password: str, quiet=True) -> None:
+    keyring.set_password("cucmtoolkit", USERNAME_MAGIC_KEY, username)
+    keyring.set_password("cucmtoolkit", username, password)
+
+
+def delete_credentials() -> None:
+    username, password = get_credentials(enable_manual_entry=False)
+    if username:
+        try:
+            keyring.delete_password("cucmtoolkit", USERNAME_MAGIC_KEY)
+        except keyring.errors.PasswordDeleteError:
+            print("could not delete username key")
+    if password:
+        try:
+            keyring.delete_password("cucmtoolkit", username)
+        except keyring.errors.PasswordDeleteError:
+            print("could not delete password key")
