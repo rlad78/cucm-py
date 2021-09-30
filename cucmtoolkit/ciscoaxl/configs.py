ba21c2786715346367bb76ea3ccf49b54829b050
rlad78 <goobysc@gmail.com>
2021-09-30 16:58:12 -0400

    options for disabling wrappers in axl

--- cucmtoolkit/ciscoaxl/configs.py
+++ cucmtoolkit/ciscoaxl/configs.py
@@ -4,3 +4,6 @@ ROOT_DIR: Path = Path(__file__).parent
 USERNAME_MAGIC_KEY: str = "73q0eWFaIE2JJw8FMNeX"
 URL_MAGIC_KEY: str = "8Cu16DGzNvunSsDNOTrO"
 DUMMY_KEY: str = "xlGoVnofkKjNSgnwA9Z7"
+
+DISABLE_FAULT_HANDLER = False
+DISABLE_SERIALIZER = False
