ed98b346003ea5a5bb1c28e540a1187e66637a29
rlad78 <goobysc@gmail.com>
2021-10-12 15:33:31 -0400

    refactor to find different element children

--- cucmtoolkit/ciscoaxl/wsdl.py
+++ cucmtoolkit/ciscoaxl/wsdl.py
@@ -2,7 +2,9 @@ from zeep import Client
 from cucmtoolkit.ciscoaxl.exceptions import WSDLException
 
 
-def get_search_criteria(z_client: Client, element_name: str) -> list[str]:
+def _get_element_child_tree(
+    z_client: Client, element_name: str, child_name: str
+) -> list[str]:
     # get element
     try:
         element = z_client.get_element(f"ns0:{element_name}")
@@ -19,11 +21,19 @@ def get_search_criteria(z_client: Client, element_name: str) -> list[str]:
 
     # find searchCriteria
     for child in elem_type.elements:
-        if child[0] == "searchCriteria":
+        if child[0] == child_name:
             criteria = child[1]  # * [0] is the name, [1] is the obj
             break
     else:
-        raise WSDLException(f"Could not find searchCriteria for type {elem_type.name}")
+        raise WSDLException(f"Could not find {child_name} for type {elem_type.name}")
 
     # extract searchCriteria
     return [e[0] for e in criteria.type.elements]
+
+
+def get_search_criteria(z_client: Client, element_name: str) -> list[str]:
+    return _get_element_child_tree(z_client, element_name, child_name="searchCriteria")
+
+
+def get_return_tags(z_client: Client, element_name: str) -> list[str]:
+    return _get_element_child_tree(z_client, element_name, child_name="returnedTags")
