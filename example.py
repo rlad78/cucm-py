474fe5cc66d06b332e9e7e6615af3e038a1302fc
rlad78 <goobysc@gmail.com>
2021-10-12 11:38:05 -0400

    formatting for use with vs interactive

--- example.py
+++ example.py
@@ -1,7 +1,12 @@
+# %%
 from cucmtoolkit import axl, get_credentials
 
 username, password = get_credentials()
 server = "ucm-01.clemson.edu"
 version = "11.5"
+dummy_sql = "select registrationdynamic.datetimestamp, registrationdynamic.fkdevice, device.pkid , device.name  from registrationdynamic inner join device on registrationdynamic.fkdevice=device.pkid where device.tkmodel='503'"
 
 ucm = axl(username, password, server, version)
+
+
+# %%
