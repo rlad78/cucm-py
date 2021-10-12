15f162dbcd9750a7b63a66aa491c9b47c90fb6bc
rlad78 <goobysc@gmail.com>
2021-10-12 15:33:39 -0400

    shorter sql

--- example.py
+++ example.py
@@ -4,9 +4,8 @@ from cucmtoolkit import axl, get_credentials
 username, password = get_credentials()
 server = "ucm-01.clemson.edu"
 version = "11.5"
-dummy_sql = "select registrationdynamic.datetimestamp, registrationdynamic.fkdevice, device.pkid , device.name  from registrationdynamic inner join device on registrationdynamic.fkdevice=device.pkid where device.tkmodel='503'"
+dummy_sql = "select name,tkmodel from TypeProduct"
 
 ucm = axl(username, password, server, version)
-
 
 # %%
