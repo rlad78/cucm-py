00f82c442e120451923d84e5929851b9be69c5d3
rlad78 <goobysc@gmail.com>
2021-10-12 12:32:11 -0400

    reliable turn off of options

--- cucmtoolkit/ciscoaxl/configs.py
+++ cucmtoolkit/ciscoaxl/configs.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from zeep.exceptions import Fault
 
 ROOT_DIR: Path = Path(__file__).parent
 USERNAME_MAGIC_KEY: str = "73q0eWFaIE2JJw8FMNeX"
@@ -7,3 +8,13 @@ DUMMY_KEY: str = "xlGoVnofkKjNSgnwA9Z7"
 
 DISABLE_FAULT_HANDLER = False
 DISABLE_SERIALIZER = False
+
+
+def turn_off_fault_handler() -> None:
+    global DISABLE_FAULT_HANDLER
+    DISABLE_FAULT_HANDLER = True
+
+
+def turn_off_serializer() -> None:
+    global DISABLE_SERIALIZER
+    DISABLE_SERIALIZER = True
