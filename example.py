5ab0f5a4d0b84b4549fd610a0646c729d8191c19
rlad78 <goobysc@gmail.com>
2021-10-15 16:11:57 -0400

    don't need ucm version anymore

--- example.py
+++ example.py
@@ -3,9 +3,7 @@ from cucmtoolkit import axl, get_credentials
 
 username, password = get_credentials()
 server = "ucm-01.clemson.edu"
-version = "11.5"
 dummy_sql = "select name,tkmodel from TypeProduct"
 
-ucm = axl(username, password, server, version)
-
+ucm = axl(username, password, server)
 # %%
