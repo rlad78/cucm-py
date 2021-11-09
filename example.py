037ac7d3c5b8f74d7f499a864752ff5766557d2a
rlad78 <goobysc@gmail.com>
2021-11-08 22:25:53 -0500

    show how to make "needed" tree

--- example.py
+++ example.py
@@ -11,3 +11,7 @@ dummy_sql = "select name,tkmodel from TypeProduct"
 
 ucm = Axl(username, password, server)
 # %%
+ele = AXLElement(ucm.zeep.get_element("ns0:addPhone"))
+ele.needed_only().print_tree(show_required=True)
+
+# %%
