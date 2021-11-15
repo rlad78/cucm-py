c92ebaf564da57fd38b969aff1da8dde83a66d03
arf <goobysc@gmail.com>
2021-11-15 10:21:47 -0500

    get the port number as well

--- cucm/debug.py
+++ cucm/debug.py
@@ -4,29 +4,61 @@ from cucm.axl.wsdl import print_element_layout
 import keyring
 import sys, os
 
-ucm = None
-while ucm is None:
-    if not (weburl := keyring.get_password("cucm-py", "webaddr")):
-        new_weburl = input("Please enter your CUCM URL (without port): ")
-        try:
-            ucm = Axl(*get_credentials(), cucm=new_weburl, verbose=True)
-            keyring.set_password("cucm-py", "webaddr", new_weburl)
-        except UCMException as e:
-            print(f"\nThat URL didn't work ({e.err})...please try again.")
-    else:
-        try:
-            ucm = Axl(*get_credentials(), cucm=weburl)
-        except UCMException as e:
-            if (
-                input(
-                    f"Stored URL '{weburl}' did not work ({e.err}).\nWant to try another? [y/n]: "
-                ).lower()
-                == "y"
-            ):
-                keyring.set_password("cucm-py", "webaddr", "")
-                continue
+
+def set_url_and_port() -> Axl:
+    valid_ucm = None
+    while valid_ucm is None:
+        if not (weburl := keyring.get_password("cucm-py", "webaddr")):
+            new_weburl = input(
+                "Please enter your CUCM URL (use ':[port]' if different than ':8443'): "
+            )
+            if (new_port := new_weburl.split(":")[-1]).isnumeric():
+                new_weburl = new_weburl[:-1]
+                port = new_port
             else:
-                raise Exception("Could not connect to UCM AXL service")
+                port = "8443"
+            try:
+                valid_ucm = Axl(
+                    *get_credentials(), cucm=new_weburl, port=port, verbose=True
+                )
+                keyring.set_password("cucm-py", "webaddr", new_weburl)
+                keyring.set_password("cucm-py", "port", port)
+            except UCMException as e:
+                print(f"\nThat URL didn't work ({e.err})...please try again.")
+        else:
+            port = keyring.get_password("cucm-py", "port")
+            try:
+                valid_ucm = Axl(*get_credentials(), cucm=weburl, port=port)
+            except UCMException as e:
+                if (
+                    input(
+                        f"Stored URL '{weburl}:{port}' did not work ({e.err}).\nWant to try another? [y/n]: "
+                    ).lower()
+                    == "y"
+                ):
+                    keyring.set_password("cucm-py", "webaddr", "")
+                    keyring.set_password("cucm-py", "port", "")
+                    continue
+                else:
+                    raise Exception("Could not connect to UCM AXL service")
+    return valid_ucm
+
+
+def get_url_and_port() -> tuple[str, str]:
+    url = keyring.get_password("cucm-py", "webaddr")
+    port = keyring.get_password("cucm-py", "port")
+    if not url:
+        raise Exception("Need to run 'poetry run test_connect' to save URL first")
+    return url, port
+
+
+def clear_url_and_port() -> None:
+    keyring.set_password("cucm-py", "webaddr", "")
+    keyring.set_password("cucm-py", "port", "")
+    print("URL and port cleared")
+
+
+ucm = set_url_and_port()
 
 
 def axl_connect() -> None:
