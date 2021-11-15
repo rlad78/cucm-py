cc48de2d49369472f9e0646906834c4b679d0780
arf <goobysc@gmail.com>
2021-11-15 10:21:55 -0500

    tests woo!!!

---
+++ tests/test_axl.py
@@ -0,0 +1,59 @@
+import pytest
+from cucm import Axl, get_credentials
+from cucm.axl.exceptions import *
+from cucm.debug import get_url_and_port
+
+
+class TestConnections:
+    def test_valid_cucm(self):
+        url, port = get_url_and_port()
+        assert Axl(*get_credentials(), cucm=url, port=port).cucm == url
+
+    def test_bad_url(self):
+        with pytest.raises(URLInvalidError):
+            Axl(*get_credentials(), "not a url silly")
+
+    def test_non_ucm_server(self):
+        with pytest.raises(UCMInvalidError):
+            Axl(*get_credentials(), "github.com", port=443)
+
+    def test_non_exist_server(self):
+        with pytest.raises(UCMNotFoundError):
+            Axl(*get_credentials(), "notanactivedomain.net", port=80)
+
+    def test_invalid_port(self):
+        url, _ = get_url_and_port()
+        with pytest.raises(UCMNotFoundError):
+            Axl(*get_credentials(), cucm=url, port=22)
+
+    def test_invalid_credentials(self):
+        url, port = get_url_and_port()
+        with pytest.raises(AXLInvalidCredentials):
+            Axl("notreal", "notreal", cucm=url, port=port)
+
+
+class TestAPI:
+    ucm = Axl(*get_credentials(), *get_url_and_port())
+
+    def test_standard_get(self):
+        result = self.ucm.get_phone(name="BOTRCARTE4")
+        assert result["description"] == "Jabber-Android-RCARTE4"
+
+    def test_correct_tags(self):
+        result = self.ucm.get_phone(name="BOTRCARTE4", return_tags=["devicePoolName"])
+        assert result["devicePoolName"] == "Clemson Campus A"
+
+    def test_invalid_tag(self):
+        with pytest.raises(TagNotValid):
+            self.ucm.get_phone(name="BOTRCARTE4", return_tags=["notRealTag"])
+
+    def test_mixed_tags(self):
+        with pytest.raises(TagNotValid):
+            self.ucm.get_phone(
+                name="BOTRCARTE4", return_tags=["description", "notRealTag"]
+            )
+
+        with pytest.raises(TagNotValid):
+            self.ucm.get_phone(
+                name="BOTRCARTE4", return_tags=["notRealTag", "description"]
+            )
