1df2ca8143532b777f1836f827d27cb415f97ffd
arf <goobysc@gmail.com>
2022-02-28 15:48:11 -0500

    freed up space for debug running

--- example.py
+++ example.py
@@ -13,14 +13,3 @@ ucm = Axl(username, password, server)
 
 
 # %%
-ele = AXLElement(ucm.zeep.get_element("ns0:addPhone"))
-ele.needed_only().print_tree(show_required=True)
-
-# %%
-print(
-    ucm.add_phone(
-        "SEP00000000EEEE", "Cisco 8845", "AXL_TEST", "8845-1LN+9SP", "Clemson Campus"
-    )
-)
-
-# %%
