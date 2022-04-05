ec94d0608225752917afc31418a67fd1931c28cb
arf <goobysc@gmail.com>
2022-04-04 20:03:15 -0400

    touchups and formatting

--- cucm/axl/asyncaxl.py
+++ cucm/axl/asyncaxl.py
@@ -24,9 +24,15 @@ if not logdir.is_dir():
     logdir.mkdir(exist_ok=True)
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
-f_format = logging.Formatter("%(asctime)s [%(levelname)s]:%(name)s:%(funcName)s - %(message)s")
+f_format = logging.Formatter(
+    "%(asctime)s [%(levelname)s]:%(name)s:%(funcName)s - %(message)s"
+)
 s_format = logging.Formatter("[%(levelname)s]:%(name)s:%(funcName)s - %(message)s")
-f_handler = RotatingFileHandler(rootcfg.LOG_DIR / f"{__name__}.log", maxBytes=(1024*1024*5), backupCount=3, )
+f_handler = RotatingFileHandler(
+    rootcfg.LOG_DIR / f"{__name__}.log",
+    maxBytes=(1024 * 1024 * 5),
+    backupCount=3,
+)
 f_handler.setLevel(logging.DEBUG)
 f_handler.setFormatter(f_format)
 s_handler = logging.StreamHandler()
@@ -35,6 +41,7 @@ s_handler.setFormatter(s_format)
 log.addHandler(f_handler)
 log.addHandler(s_handler)
 log.info(f"----- NEW {__name__} SESSION -----")
+
 
 @unique
 class APICall(Enum):
@@ -48,12 +55,15 @@ class APICall(Enum):
     RESET = "RESET"
     WIPE = "WIPE"
 
+
 TASK_COUNTER = 0
+
 
 class AsyncClientExt(AsyncClient):
     """Extended zeep.AsyncClient with upstream fix yet to be merged.
     When change at https://github.com/mvantellingen/python-zeep/pull/1202 is merged, this should no longer be needed.
     """
+
     def create_service(self, binding_name, address):
         """Create a new AsyncServiceProxy for the given binding name and address.
         :param binding_name: The QName of the binding
@@ -71,9 +81,17 @@ class AsyncClientExt(AsyncClient):
 
 
 class AsyncAXL:
-    """An asynchronous interface for the AXL API.
-    """
-    def __init__(self, username: str, password: str, server: str, port: str = "8443", *, version: str = None) -> None:
+    """An asynchronous interface for the AXL API."""
+
+    def __init__(
+        self,
+        username: str,
+        password: str,
+        server: str,
+        port: str = "8443",
+        *,
+        version: str = None,
+    ) -> None:
         """Connect to your UCM's AXL server.
 
         Args:
@@ -92,12 +110,14 @@ class AsyncAXL:
             UDSParseError: Could not parse the UCM version number from UDS. Please contact the maintainer of this project if you get this exception
             UCMVersionError: The UCM version is either invalid or unsupported
             UCMVersionInvalid: An unsupported UCV version is detected
-            AXLInvalidCredentials: 
-            AXLConnectionFailure: 
-            AXLNotFoundError: 
+            AXLInvalidCredentials:
+            AXLConnectionFailure:
+            AXLNotFoundError:
             AXLException: An unknown error is preventing a connection to the AXL server
         """
-        log.info(f"Attempting to verify {server} on port {port} is a valid UCM server...")
+        log.info(
+            f"Attempting to verify {server} on port {port} is a valid UCM server..."
+        )
         try:
             ucm_is_valid: bool = validate_ucm_server(server, port)
         except (
@@ -111,14 +131,16 @@ class AsyncAXL:
         if not ucm_is_valid:
             log.error(f"Could not connect to {server}, unknown error occured")
             raise ConnectionError()
-        
+
         if version is not None:
             if (match := re.search(r"^(\d{1,2}(?:\.\d{1})?)", version)) is None:
                 raise InvalidArguments(f"{version=} is not a valid UCM version")
             parsed_version = match.group(0)
             log.debug(f"{version=}, {parsed_version=}")
             if "." not in parsed_version:
-                log.debug(f"Supplied UCM version '{parsed_version}' didn't have a decimal place, adding '.0' to end.")
+                log.debug(
+                    f"Supplied UCM version '{parsed_version}' didn't have a decimal place, adding '.0' to end."
+                )
                 parsed_version += ".0"
             cucm_version = parsed_version
             log.debug(f"Using user supplied version '{parsed_version}'")
@@ -129,7 +151,7 @@ class AsyncAXL:
                 log.exception(err)
                 raise
             log.debug(f"Found UCM version: {cucm_version}")
-        
+
         wsdl_path = cfg.AXL_DIR / "schema" / cucm_version / "AXLAPI.wsdl"
         log.debug(f"WSDL Path: {wsdl_path}")
         if not wsdl_path.parent.is_dir():
@@ -145,20 +167,41 @@ class AsyncAXL:
         if not axl_is_valid:
             log.error("Could not connect to the AXL API for an unknown reason")
             raise AXLException()
-        
+
         httpx_client = httpx.AsyncClient(auth=(username, password))
         settings = Settings(
             strict=False, xml_huge_tree=True, xsd_ignore_sequence_order=True
         )
-        self.zeep = AsyncClientExt(str(wsdl_path), settings=settings, transport=AsyncTransport(client=httpx_client))
+        self.zeep = AsyncClientExt(
+            str(wsdl_path),
+            settings=settings,
+            transport=AsyncTransport(client=httpx_client),
+        )
         self.aclient = self.zeep.create_service(
             "{http://www.cisco.com/AXLAPIService/}AXLAPIBinding",
             f"https://{server}:{port}/axl/",
         )
         log.info(f"AXL Async client created for {server}")
 
+    ###############################
+    # ==== TEMPLATES & HELPERS ====
+    ###############################
 
-    async def _generic_soap_call(self, element: str, action: APICall, children: list[str] = None, **kwargs):
+    async def _generic_soap_call(
+        self, element: str, action: APICall, children: list[str] = None, **kwargs
+    ):
+        """Base logic for making a call to the AXL SOAP API. Used when no other logic is needed to perform the required action.
+
+        Args:
+            element (str): Name of the element being used for the call, i.e. getPhone
+            action (APICall): Type of call being made (usually the prefix of the element)
+            children (list[str], optional): Keys of the returned nested object that lead to the desired information. Defaults to None.
+
+        Returns:
+            zeep.object: When valid data is found (@serialize will turn this into a dict)
+            dict: Empty dict when GET call is successful but no data is found
+            list: Empty list when LIST call is successful but no items are returned
+        """
         if (func := getattr(self.aclient, element, None)) is None:
             raise DumbProgrammerException(f"{element} is not an AXL element")
         if children is None:
@@ -166,30 +209,77 @@ class AsyncAXL:
 
         global TASK_COUNTER
         TASK_COUNTER += 1
-        current_task = str(TASK_COUNTER)
+        current_task = f"[{str(TASK_COUNTER).zfill(4)}]"
 
-        log.info(f"({current_task.zfill(4)}) Performing a(n) {action.value} for {kwargs}")
+        log.info(f"{current_task} Performing {action.value} for {kwargs}")
         try:
             results = await func(**kwargs)
-            for child in children:
-                results = results[child]
-            log.info(f"({current_task.zfill(4)}) Completed successfully")
-            return results
-        except Fault:
+        except Fault as e:
             if action is APICall.GET:
-                log.info(f"({current_task.zfill(4)}) Completed, but results empty")
-                return {}
+                if "was not found" in e.message:
+                    log.info(f"{current_task} Completed, but could not find item")
+                    return {}
+                else:
+                    log.exception()
+                    raise
             elif action is APICall.LIST:
-                log.info(f"({current_task.zfill(4)}) Completed, but list empty")
+                log.info(f"{current_task} Completed, but list empty")
                 return []
             else:
                 log.exception()
-                raise
+                raise e
+
+        try:
+            for child in children:
+                results = results[child]
         except KeyError:
             raise DumbProgrammerException(f"Invalid children for {element}: {children}")
-        
+        except TypeError:
+            if action is APICall.GET:
+                log.info(f"{current_task} Completed, but no item was returned")
+                return {}
+            elif action is APICall.LIST:
+                log.info(f"{current_task} Completed, but no items were returned")
+                return []
+            else:
+                raise DumbProgrammerException(
+                    f"Children {children} lead to 'None' result"
+                )
+
+        log.info(f"{current_task} Completed successfully")
+        return results
+
+    async def _generic_soap_with_uuid(
+        self,
+        element: str,
+        action: APICall,
+        base_field: str,
+        children: list[str] = None,
+        **kwargs,
+    ):
+        if base_field not in kwargs.keys() or "uuid" not in kwargs.keys():
+            raise DumbProgrammerException(
+                f"'{base_field}' was not a provided field in kwargs"
+            )
+
+        uuid = kwargs["uuid"]
+        base = kwargs[base_field]
+        if base and not uuid:
+            kwargs.pop("uuid")
+        elif uuid:
+            kwargs.pop(base_field)
+        else:
+            raise InvalidArguments(f"No {base_field} or uuid values found in kwargs")
+
+        return await self._generic_soap_call(element, action, children, **kwargs)
 
     @serialize
     @check_tags("getPhone")
     async def get_phone(self, name: str, *, return_tags: list[str] = None) -> dict:
-        return await self._generic_soap_call("getPhone", APICall.GET, ["return", "phone"], name=name, returnedTags=return_tags)
+        return await self._generic_soap_call(
+            "getPhone",
+            APICall.GET,
+            ["return", "phone"],
+            name=name,
+            returnedTags=return_tags,
+        )
